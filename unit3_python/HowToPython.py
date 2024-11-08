a325803530e897ccfae9bf0029a26b5fb7e070d9
gasmikel1 <mgaskins845@boyslatin.net>
2024-11-08 15:06:47 +0000

    AsyncActivtyComplete

--- unit3_python/HowToPython.py
+++ unit3_python/HowToPython.py
@@ -6,3 +6,10 @@ print("A") if a > b else print("=") if a == b else print("B")
 import random
 
 print(random.randrange(1, 100))
+
+
+follows=233
+following=1234
+totalFollows= follows + following
+
+print(totalFollows)
