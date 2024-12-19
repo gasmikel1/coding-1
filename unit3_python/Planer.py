d27d96d3b60c36369afc078674dfc3bc24656707
gasmikel1 <mgaskins845@boyslatin.net>
2024-12-19 15:24:17 +0000

    planerFinished

--- unit3_python/Planer.py
+++
@@ -1,8 +0,0 @@
-def planer(event):
-    if event == 'active':
-        print('you have a event on this day')
-    else:
-        print('you dont have a event here')
-planer('active')
-
-# instructor note l
---
+++ unit3_python/holidayExtraCredit.py
@@ -0,0 +1,22 @@
+def planer(event):
+    if event == '12/25':
+        print('you have a event today')
+    elif event == '12/23':
+        print('today is shopping day')
+    elif event == '12/31':
+        print('befroe new years party!')
+    elif event =='12/27':
+        print('clean up holiday props')
+    elif event =='1/1':
+        print( 'New years party! And dont forget to wacth the ball drop ')
+    else:
+        print('you dont have a event today')
+planer('12/30')
+
+
+
+
+
+
+
+# instructor note l
