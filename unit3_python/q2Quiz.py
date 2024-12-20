fb777b615d27c696a21c3504e6bb668708786ebf
gasmikel1 <mgaskins845@boyslatin.net>
2024-12-20 15:24:48 +0000

    Q2Quiz

---
+++ unit3_python/q2Quiz.py
@@ -0,0 +1,28 @@
+# In your own words, describe what a function is: A function is what you are telling the compulter what to do.
+
+#In your own words, describe the difference between a function parameter and a function argument. 
+#'A function argument is like a conversation you start by talking about one thihg than to the end that one thing gets brong up again
+#'a function parameter is a argument but with built in valuse.
+
+#In your own words, describe what an if/ else conditional statement is.
+
+#'if, as in if the time is 12 then ring the bell.
+#'esle, as in if not
+
+#What is an integer data type
+#' a number '
+
+#What is a boolean data type
+#' true or false'
+
+#Which operator would work best for the following function: An age verification function that needs to check if a user is a certain age and older to purchase a video game
+#'  logic'
+
+#8   
+
+def password():
+    if password <6  >10:
+        print('password')
+    else:
+        print(' cant use this password')
+password(1):        
