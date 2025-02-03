a2da6865f7b9045b7a2c129d49e68684e7011755
gasmikel1 <mgaskins845@boyslatin.net>
2025-02-03 15:18:46 +0000

    classWorkMonday

--- unit3_python/asyncActivityJan31.py
+++ unit3_python/asyncActivityJan31.py
@@ -4,11 +4,25 @@
 #if a student enters the number 80, the program should give back the letter grade "B".
 #Your program should then repeat by  asking the user to enter another numerical grade. Each time a user
 #enters another numerical grade, it should perform some calculation to average out the grades and show the new letter grade,
-# For example: if the user enters the number 70, and the 100, they're average should be an 85, which is 
+# For example: if the user enters the number 70, and the 100, they're average should be an 85, which i 
+
+"the promt wants me to creat looping program that well take in a number," 
+"and the program well retrun the aveage letter grade."
+
+#how do we calutate the avarge
+grade= [50,100,50,70,70,80,90,70]
+grades=508# needed to add
+scoreOfGrade=len(grade)#needed to count
+grades/scoreOfGrade#divid
 
 
+"promt said repeat=loop" 
+"enter numarical grade= float /datatyp"
+"numberenter solve= arithmitic ops"
+"return a tetter grade =give bake a string " 
+" compare the grade to a letter garde if/else </>"
+"floaets and or  integers are to be taking"
+"should be able to give back a letter grade based on the numbers that was given"
 
-def grades(grade):
-  
-    
-    
+
+#list
