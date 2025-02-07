ab866a6a3668d6eee72c96a5b30a0b9f4c31084e
gasmikel1 <mgaskins845@boyslatin.net>
2025-02-07 14:55:05 +0000

    feb_2_2025

---
+++ unit3_python/AsyncActivetyFeb2.py
@@ -0,0 +1,10 @@
+# a while loops = if this is true keep running
+
+#If loop = for every this thing keep loopping
+
+# youll use while loops to for like a password program
+
+#youll use if loops for like statements "if this equals that keep running". 
+
+#first for this program Ill use a list A equals, and what not, then I need to give users the option to 
+#input a number and it would print out a letter grade.
